64757e8341ca32b3f1a7fb1ace89e4b05df2e21b
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-10-06 12:06:43 -0300

    feat: created CustomSelect component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
+import { useState } from "react"
 import { AllCustomerModal } from "./components/all-customer-modal"
+import { CustomerSelect } from "./components/customer-select";
 
 
 export function App() {
 
+ const [ openRegister, setOpenRegister ] = useState(false);
+
 
   return (
     <div className="flex flex-col gap-3 justify-center items-center h-screen">
-      
-      <AllCustomerModal />
+      {openRegister ? (
+        <div className="h-auto w-1/3 border-2 py-2 bg-white rounded-lg border-gray-800">
+          <h2 className="text-center font-medium text-2xl mb-3">Cadastre um novo cliente</h2>
+          <div className="flex flex-col gap-2 mx-5">
+            <label>Nome: </label>
+            <input type="text" className="border-2 border-black focus:outline-none focus:border-[3px] rounded-md"/>
+            <label>Email: </label>
+            <input type="email" className="border-2 border-black focus:outline-none rounded-md" />
+            <div className="flex justify-center">
+              <CustomerSelect />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <AllCustomerModal />
+      )}
 
       <div>
-        <button className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold rounded-lg border-black border-[3px] px-8 py-3 text-3xl">
-          Cadastrar
-        </button>
+        {openRegister ? (
+          <button
+            onClick={() => setOpenRegister(false)}
+            className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold rounded-lg border-black border-[3px] px-8 py-3 text-3xl"
+          >
+            Cadastrar
+          </button>
+        ) : (
+          <button 
+            onClick={() => setOpenRegister(true)}
+            className="bg-yellow-400 hover:bg-yellow-500 text-black font-bold rounded-lg border-black border-[3px] px-8 py-3 text-3xl">
+            Novo Cliente
+          </button>
+        )}
       </div>
     </div>
   )
---
+++ src/components/customer-select.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+export function CustomerSelect(){
+    const [selectedOption, setSelectedOption] = useState("ativo");
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const handleClick = (value: string) => {
+        setSelectedOption(value);
+        setIsOpen(false);
+    };
+
+    return(
+        <div className="relative w-1/4">
+            <div 
+                className="border border-gray-300 rounded-lg p-2 cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+                >
+                {selectedOption === "ativo" ? (
+                <div className="flex items-center">
+                    <div className="w-3 h-3 rounded-full bg-green-500 mr-2" />
+                    Ativo
+                </div>
+                ) : (
+                <div className="flex items-center">
+                    <div className="w-3 h-3 rounded-full bg-red-500 mr-2" />
+                    Desativo
+                </div>
+                )}
+            </div>
+            {isOpen && (
+                <div className="absolute left-0 top-full mt-2 w-full border border-gray-300 bg-white rounded-lg shadow-lg">
+                    <div
+                    className="flex items-center p-2 cursor-pointer hover:bg-gray-100"
+                    onClick={() => handleClick("ativo")}
+                    >
+                    <div className="w-3 h-3 rounded-full bg-green-500 mr-2" />
+                    Ativo
+                    </div>
+                    <div
+                    className="flex items-center p-2 cursor-pointer hover:bg-gray-100"
+                    onClick={() => handleClick("desativo")}
+                    >
+                    <div className="w-3 h-3 rounded-full bg-red-500 mr-2" />
+                    Desativo
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+};
